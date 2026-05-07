d3846752536ae9351b0035d7efc1d6efb01d0f05
Erick Reyes <erickjreyesc@gmail.com>
2026-05-06 19:49:50 -0500

    Actualización original

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,6 @@
 
 namespace App\Providers;
 
-use App\Billing\BankPaymentGateway;
-use App\Billing\CreditPaymentGateway;
-use App\Billing\IPaymentGatewayContract;
-use App\Services\DeliveryService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,26 +9,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
-        $this->app->singleton(IPaymentGatewayContract::class, function ($app) {
-
-            if (request()->has('credit')) {
-                return new CreditPaymentGateway('USD');
-            }
-
-            return new BankPaymentGateway('USD');
-        });
-    }
+    public function register(): void {}
 
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-
-        $this->app->singleton(DeliveryService::class, function ($app) {
-            return new DeliveryService('Denmark', 'by_air');
-        });
-    }
+    public function boot(): void {}
 }
